d99bc386dd98bda888aa61097d7a25014b203551
Tom Payne <twpayne@gmail.com>
2014-02-28 20:32:31 +0100

    Initial commit

---
+++ gohttpd.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func addACAOHeader(value string, h http.Handler) http.Handler {
+	if value == "" {
+		return h
+	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", value)
+		h.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+
+	var (
+		acao   = flag.String("acao", "*", "Access-Control-ALlow-Origin")
+		addr   = flag.String("addr", "127.0.0.1:8080", "addr")
+		root   = flag.String("root", ".", "root")
+		prefix = flag.String("prefix", "/", "prefix")
+	)
+	flag.Parse()
+
+	http.Handle(*prefix, addACAOHeader(*acao, http.StripPrefix(*prefix, http.FileServer(http.Dir(*root)))))
+
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
