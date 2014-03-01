91c9b162ad22373f8fa8eedb4055719cb4d9b7f2
Tom Payne <twpayne@gmail.com>
2014-03-01 16:19:58 +0100

    Add MBTiles serving

--- gohttpd.go
+++ gohttpd.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"github.com/gorilla/handlers"
+	"github.com/twpayne/gombtiles/mbtiles"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 func addACAOHeader(value string, h http.Handler) http.Handler {
@@ -18,13 +20,18 @@ func addACAOHeader(value string, h http.Handler) http.Handler {
 	})
 }
 
+func stripExt(path string) string {
+	return path[:len(path)-len(filepath.Ext(path))]
+}
+
 func main() {
 
 	var (
-		acao   = flag.String("acao", "*", "Access-Control-Allow-Origin")
-		addr   = flag.String("addr", "127.0.0.1:8080", "addr")
-		root   = flag.String("root", ".", "root")
-		prefix = flag.String("prefix", "/", "prefix")
+		acao     = flag.String("acao", "*", "Access-Control-Allow-Origin")
+		addr     = flag.String("addr", "127.0.0.1:8080", "addr")
+		root     = flag.String("root", ".", "root")
+		mbtiles_ = flag.String("mbtiles", "", "MBTiles")
+		prefix   = flag.String("prefix", "/", "prefix")
 	)
 	flag.Parse()
 
@@ -33,6 +40,22 @@ func main() {
 			addACAOHeader(*acao,
 				http.StripPrefix(*prefix,
 					http.FileServer(http.Dir(*root))))))
+
+	if *mbtiles_ != "" {
+		if mbtilesBase := filepath.Base(*mbtiles_); mbtilesBase != "." {
+			mbtilesPrefix := "/" + stripExt(mbtilesBase) + "/"
+			ts, err := mbtiles.NewTileServer(*mbtiles_)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer ts.Close()
+			http.Handle(mbtilesPrefix,
+				handlers.LoggingHandler(os.Stdout,
+					addACAOHeader(*acao,
+						http.StripPrefix(mbtilesPrefix,
+							ts))))
+		}
+	}
 
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal(err)
